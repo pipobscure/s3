84919a749b2183f6b05e37e450463fb5b8a98270
Philipp Dunkel <pdunkel1@bloomberg.net>
2024-01-18 14:32:23 +0000

    initial version

---
+++ README.md
@@ -0,0 +1,36 @@
+# @pipobscure/s3
+
+This is a simple S3 library because the one from amazon has so many dependencies and is so heavy that it's no longer auditable and exceeds reason.
+
+This is simple and low on dependecies.
+
+## API
+
+```typescript
+import { S3 } from '@pipobscure/s3';
+
+const s3options: {
+	key: string; // AWS-Key
+	secret: string; // AWS-Secret
+	endpoint: string; // the endpoint to use (if host based does not include the bucket)
+	bucket: string; // the bucket name
+	signal?: AbortSignal; // an optional AbortSignal
+	hostBased?: boolean; // use hostbased API (defaults to path-based)
+} = {...};
+const s3client = new S3(s3options);
+
+const content = await s3client.get('/my/resource/name.file');
+await s3client.put('/my/resource/name.file', content, {...}); // the optional last options are extra properties
+await s3client.delete('/my/resource/name.file');
+const list = await s3client.list('/my/resource', '/'); // separator '/' is optional
+await s3client.copy('/my/resource/destination.file', 'otherbucket/my/source/file.dat');
+```
+## License
+
+ISC License
+
+Copyright 2024 Philipp Dunkel
+
+Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
---
+++ src/s3.ts
@@ -0,0 +1,175 @@
+import * as HTTPS from 'node:https';
+import * as CRYPTO from 'node:crypto';
+import { IncomingHttpHeaders } from 'node:http';
+import * as XML from 'xml-js';
+
+export interface S3Options {
+	key: string;
+	secret: string;
+	endpoint: string;
+	bucket: string;
+	signal?: AbortSignal;
+	hostBased?: boolean;
+}
+export type S3Method = 'GET' | 'PUT' | 'DELETE' | 'HEAD';
+
+const QUERY_WHITELIST = ['acl', 'delete', 'lifecycle', 'location', 'logging', 'notification', 'partNumber', 'policy', 'requestPayment', 'torrent', 'uploadId', 'uploads', 'versionId', 'versioning', 'versions', 'website'];
+
+export type AWSProperty = '';
+export type AWSProperties = Record<AWSProperty, string>;
+
+export class S3 {
+	#key: string;
+	#secret: string;
+	#endpoint: string;
+	#bucket: string;
+	#signal?: AbortSignal;
+	#hostBased: boolean;
+	constructor(options: S3Options) {
+		const { key, secret, endpoint, bucket, signal, hostBased = false } = options;
+		this.#key = key;
+		this.#secret = secret;
+		this.#endpoint = endpoint;
+		this.#bucket = bucket;
+		this.#signal = signal;
+		this.#hostBased = hostBased;
+	}
+	#url(filename: string) {
+		if (this.#hostBased) {
+			return new URL(`/${filename}`.split(/\/+/).join('/'), `https://${this.#bucket}.${this.#endpoint}`);
+		} else {
+			return new URL(`/${this.#bucket}/${filename}`.split(/\/+/).join('/'), `https://${this.#endpoint}`);
+		}
+	}
+	#request(method: S3Method, url: URL, headers: Record<string, string>, content?: Buffer) {
+		return new Promise<{ content: Buffer; headers: IncomingHttpHeaders }>((resolve, reject) => {
+			for (const key of url.searchParams.keys()) {
+				if (!QUERY_WHITELIST.includes(key)) url.searchParams.delete(key);
+			}
+			const date = new Date();
+			const resource = `${url.pathname}${url.search}`;
+			headers.Date = date.toUTCString();
+			headers.Host = url.host;
+			if (content) {
+				headers['Content-Length'] = `${content.byteLength}`;
+				headers['Content-MD5'] = CRYPTO.createHash('md5').update(content).digest('base64');
+				headers['Content-Type'] = headers['Content-Type'] ?? 'application/octet-stream';
+			}
+			const signature = sign(this.#secret, method, headers['Content-MD5'] ?? '', headers['Content-Type'] ?? '', headers.Date, canonicalizeHeaders(headers), resource);
+			headers.Authorization = `AWS ${this.#key}:${signature}`;
+
+			HTTPS.request(
+				url,
+				{
+					method,
+					headers,
+					signal: this.#signal,
+				},
+				async (response) => {
+					try {
+						const content = await body(response);
+						if (!response.statusCode) return reject(new Error(`No Repsonse`));
+						if (response.statusCode < 200 || response.statusCode > 399) return reject(new Error(`HTTP ${response.statusCode}`));
+						if (response.statusCode > 299) return reject(new Error('cannot redirect'));
+						resolve({ content, headers: response.headers });
+					} catch (err) {
+						return reject(err);
+					}
+				},
+			)
+				.on('error', (err) => reject(err))
+				.end(content);
+		});
+	}
+	async get(resource: string): Promise<Buffer> {
+		const headers: Record<string, string> = {};
+		const { content } = await this.#request('GET', this.#url(resource), headers);
+		return content;
+	}
+	async put(resource: string, content: Buffer | string | any, properties: Partial<AWSProperties & { type: string }> = {}) {
+		let { type, ...hdrs } = properties;
+		if ('string' === typeof content) {
+			content = Buffer.from(content, 'utf-8');
+			type = type ?? 'text/plain; charset=utf-8';
+		}
+		if (!Buffer.isBuffer(content)) {
+			content = Buffer.from(JSON.stringify(content), 'utf-8');
+			type = type ?? 'application/json; charset=utf-8';
+		}
+		const { headers } = await this.#request('PUT', this.#url(resource), Object.assign(hdrs, { type: type ?? 'application/octet-stream' }), content);
+		return headers;
+	}
+	async del(resource: string) {
+		const hdrs: Record<string, string> = {};
+		const { headers } = await this.#request('DELETE', this.#url(resource), hdrs);
+		return headers;
+	}
+	async list(prefix?: string, delimiter?: string, options?: Record<string, string>) {
+		const url = this.#url('/');
+		if (prefix) {
+			url.searchParams.set('prefix', prefix);
+			url.searchParams.set('delimiter', delimiter ?? '/');
+		}
+		if (options) {
+			for (const [key, val] of Object.entries(options)) url.searchParams.set(key, val);
+		}
+		const { content } = await this.#request('GET', url, {});
+		const data = XML.xml2js(content.toString('utf-8'), {
+			compact: true,
+			trim: true,
+			ignoreDeclaration: true,
+			ignoreInstruction: true,
+			ignoreAttributes: true,
+			ignoreComment: true,
+			alwaysArray: true,
+		}) as any;
+		const contents = data?.ListBucketResult?.[0]?.Contents ?? [];
+		return contents.map((item: any) => {
+			const name = item?.Key?.[0]?._text?.join('');
+			const modified = item?.LastModified?.[0]?._text?.join('') ?? new Date().toISOString();
+			const size = +(item?.Size?.[0]?._text?.join('') ?? 0);
+			return { name, size, modified };
+		});
+	}
+	async copy(resource: string, source: string, properties?: Partial<AWSProperties>) {
+		const result = await this.#request(
+			'PUT',
+			this.#url(resource),
+			Object.assign(
+				{
+					'x-amz-copy-source': source,
+				},
+				properties ?? {},
+			),
+		);
+		if (result.content.byteLength) {
+			return Object.assign(XML.xml2json(result.content.toString('utf-8')), result.headers);
+		} else {
+			return result.headers;
+		}
+	}
+}
+
+function canonicalizeHeaders(headers: Record<string, string>) {
+	const result: [string, string][] = [];
+	for (let [key, val] of Object.entries(headers)) {
+		key = key.toLowerCase();
+		if (!key.startsWith('x-amz') || key === 'x-amz-date') continue;
+		result.push([key, val]);
+	}
+	result.sort((a, b) => (a[0] > b[0] ? 1 : -1));
+	return result.map((...args) => args.join(':'));
+}
+function sign(secret: string, method: S3Method, md5: string, type: string, date: string, awsheaders: string[], resource: string) {
+	const message = [method, md5, type, date, ...awsheaders, resource].join('\n');
+	return CRYPTO.createHmac('sha1', secret).update(Buffer.from(message, 'utf-8')).digest('base64');
+}
+async function body(body: AsyncIterable<Buffer>) {
+	const buf = [];
+	let len = 0;
+	for await (const chunk of body) {
+		buf.push(chunk);
+		len += chunk.byteLength;
+	}
+	return Buffer.concat(buf, len);
+}
